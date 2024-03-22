91e8ca0a67b9a6940e91da0501743230f72bfcbc
noahlago <ndl3389@rit.edu>
2024-03-22 12:11:58 -0400

    finished setting up class structures

---
+++ calculator/src/main/java/calculator/Distributions/BinomialDistribution.java
@@ -0,0 +1,61 @@
+package calculator.Distributions;
+
+import calculator.Distribution;
+
+public class BinomialDistribution implements Distribution{
+
+    @Override
+    public double popDev() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'popDev'");
+    }
+
+    @Override
+    public double popMean() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'popMean'");
+    }
+
+    @Override
+    public double sampleDev() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sampleDev'");
+    }
+
+    @Override
+    public double sampleMean() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'sampleMean'");
+    }
+
+    @Override
+    public void setN(int n) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setN'");
+    }
+
+    @Override
+    public void setSampleDev(double dev) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSampleDev'");
+    }
+
+    @Override
+    public void setSampleMean(double mean) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSampleMean'");
+    }
+
+    @Override
+    public void setPopDev(double dev) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPopDev'");
+    }
+
+    @Override
+    public void setPopMean(double mean) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPopMean'");
+    }
+    
+}
