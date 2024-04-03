f4a2e680f763484fd85fb224e99656ed7236370a
noahlago <ndl3389@rit.edu>
2024-04-02 23:46:05 -0400

    refactored Distribution interface into an abstract class

--- calculator/src/main/java/calculator/Distribution.java
+++ calculator/src/main/java/calculator/Distribution.java
@@ -1,32 +1,47 @@
 package calculator;
 
-public interface Distribution {
+public abstract class Distribution {
+    private double popDev;
+    private double popMean;
+    private double sampleDev;
+    private double sampleMean;
+    private int n;
 
     //Calculate population standard deviation
-    public double popDev();
+    public abstract double popDev();
 
     //Calculate population mean/true mean
-    public double popMean();
+    public abstract double popMean();
 
     //Calculate sample standard deviation
-    public double sampleDev();
+    public abstract double sampleDev();
 
     //Calculate sample mean
-    public double sampleMean();
+    public abstract double sampleMean();
 
     //Set sample size (N)
-    public void setN(int n);
+    public void setN(int n){
+        this.n = n;
+    }
 
     //Set sample standard deviation
-    public void setSampleDev(double dev);
+    public void setSampleDev(double dev){
+        this.sampleDev = dev;
+    }
 
     //Set sample mean
-    public void setSampleMean(double mean);
+    public void setSampleMean(double mean){
+        this.sampleMean = mean;
+    }
 
     //Set population standard deviation
-    public void setPopDev(double dev);
+    public void setPopDev(double dev){
+        this.popDev = dev;
+    }
 
     //Set population mean/true mean
-    public void setPopMean(double mean);
+    public void setPopMean(double mean){
+        this.popMean = mean;
+    }
 
 }
