6bc8718352060855be3af0fc78d4a6696ba2aa5b
noahlago <ndl3389@rit.edu>
2025-01-03 15:52:14 -0500

    Added documentation to the implemented normality tests

--- calculator/src/main/java/calculator/IdentifyDistributions/KolmogrovSmirnovIdentifier.java
+++ calculator/src/main/java/calculator/IdentifyDistributions/KolmogrovSmirnovIdentifier.java
@@ -5,25 +5,38 @@ import org.apache.commons.math3.stat.inference.KolmogorovSmirnovTest;
 
 import calculator.SampleUtils;
 
-
+/**
+ * This class is an implementation of the ComplexIdentifier, using the Kolmogrov Smirnov test to check for normality
+ * @author Noah Lago ndl3389@rit.edu
+ */
 public class KolmogrovSmirnovIdentifier implements ComplexIdentifier{
 
+    //Pass/fail threshold to determine if the data is "close enough" to normal distribution
     double threshold = 0.05;
 
+    /**
+     * This method checks whether the provided raw data follows a normal distribution by comparing
+     * the raw data to a reference normal distribution with the same mean and standard deviation as the raw data. 
+     * @param data represents the raw data to be evaluated as a double array
+     * @return true if the data closely follows a normal distribution
+     */
     @Override
     public boolean checkNormality(double[] data) {
         double mean = SampleUtils.meanFromVals(data);
 
         double var = 0;
 
+        //Sum variance to use for standard deviation calc
         for (double d : data) {
             var += Math.pow(d - mean, 2);
         }
 
         double stdDev = Math.sqrt(var / data.length);
 
+        //Reference normal distribution
         NormalDistribution normal = new NormalDistribution(mean, stdDev);
 
+        //Perform test to check how closely the data mimics a normal distribution with the same mean and standard deviation
         double result = (new KolmogorovSmirnovTest()).kolmogorovSmirnovTest(normal, data);
 
         return result > threshold;
--- calculator/src/main/java/calculator/IdentifyDistributions/ShapiroWilkIdentifier.java
+++ calculator/src/main/java/calculator/IdentifyDistributions/ShapiroWilkIdentifier.java
@@ -1,19 +1,32 @@
 package calculator.IdentifyDistributions;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 
-
+/**
+ * This class is an implementation of the ComplexIdentifier, using the Shapiro Wilk test to check for normality
+ * @author Noah Lago ndl3389@rit.edu
+ */
 public class ShapiroWilkIdentifier implements ComplexIdentifier {
 
+    /**
+     * This method conducts a Shapiro Wilk test on the raw data provided, using the skewness and 
+     * kurtosis of the data to evaluate whether it can be stated to approximately follow a normal distribution
+     * @param data array of double values representing the raw data
+     * @return true if the raw data approximately follows a normal distribution
+     */
+    @Override
     public boolean checkNormality(double[] data){
         DescriptiveStatistics dsStats = new DescriptiveStatistics();
         
+        //Convert to data structure usable by the library
         for (double d : data) {
             dsStats.addValue(d);
         }
 
+        //Calculate skewness and kurtosis 
         double skewness = dsStats.getSkewness();
         double kurtosis = dsStats.getKurtosis();
 
+        //check skewness and kurtosis against thresholds
         return (Math.abs(skewness) < 2 && Math.abs(kurtosis) < 3);
     }
 }
